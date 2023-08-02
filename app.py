926f977544e3015765990d19f8210c910c146022
dhruv7soni <90857169+dhruv7soni@users.noreply.github.com>
2023-08-02 21:37:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+#pip install --upgrade watchdog
+
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+app = Flask(__name__)
+model = pickle.load(open("model.pkl","rb"))
+
+@app.route('/predict', methods = ["POST"])
+def predict():
+    json_ = request.json
+    query_df = pd.DataFrame(json_)
+    prediction = model.predict(query_df)
+    return jsonify({"Prediction" : list(prediction)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    
